4e147c1b37e360de4392ebc4a228fd1506a52b3a
xeo3221 <95309495+xeo3221@users.noreply.github.com>
2024-04-23 23:29:19 +0200

    feat: randomize colors

--- src/components/SingleLight.jsx
+++ src/components/SingleLight.jsx
@@ -1,11 +1,30 @@
 import { useState } from "react";
 import LightTurnedOff from "/images/light-turned-off.svg";
 import LightBlue from "/images/light-blue.svg";
+import LightGreen from "/images/light-green.svg";
+import LightOrange from "/images/light-orange.svg";
+import LightPink from "/images/light-pink.svg";
+import LightRed from "/images/light-red.svg";
+import LightYellow from "/images/light-yellow.svg";
+
+const lights = [
+  LightBlue,
+  LightGreen,
+  LightOrange,
+  LightPink,
+  LightRed,
+  LightYellow,
+];
 
 export const SingleLight = () => {
   const [isOn, setIsOn] = useState(true);
 
-  const imageSrc = isOn ? LightBlue : LightTurnedOff;
+  const getRandomIndex = () => {
+    return Math.floor(Math.random() * lights.length);
+  };
+  const randomIndex = getRandomIndex();
+
+  const imageSrc = isOn ? lights[randomIndex] : LightTurnedOff;
 
   const handleToggle = () => {
     setIsOn(!isOn);
